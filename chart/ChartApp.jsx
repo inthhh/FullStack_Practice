f1c63ce58b86a0497cb160913cd8ea4b96e83eab
Heather Jeong <91872300+inthhh@users.noreply.github.com>
2025-02-13 17:34:12 +0900

    React-Chart.js 연습코드

---
+++ chart/ChartApp.jsx
@@ -0,0 +1,16 @@
+// import "./App.css";
+import BarChart from "./BarChart";
+import LineChart from "./LineChart";
+import PieChart from "./PieChart";
+
+const ChartApp = function ChartApp() {
+  return (
+    <>
+      <BarChart />
+      <LineChart />
+      <PieChart />
+    </>
+  );
+};
+
+export default ChartApp;
